50f40a9dfebc9c94aede8cc010234914afca0912
SON7AE <bobsot0225@gmail.com>
2023-10-30 14:51:54 +0900

    sendFile로 이미지 전송하기

--- Node/express/controllers/post.controller.js
+++ Node/express/controllers/post.controller.js
@@ -1,5 +1,8 @@
+const path = require("path")
+
 function getPost(req, res) {
-    res.send("<div><h1>POST TITLE</h1><p>THIS IS A POST</p></div>")
+    // res.send("<div><h1>POST TITLE</h1><p>THIS IS A POST</p></div>")
+    res.sendFile(path.join(__dirname, "..", "public", "images", "logo.png"))
 }
 
 module.exports = { getPost }
