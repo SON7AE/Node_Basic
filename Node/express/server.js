8462dfdde9cc8f8e0399aa48735e36aed4ebb13e
SON7AE <bobsot0225@gmail.com>
2023-10-30 10:29:54 +0900

    POST 요청하는 방법

--- Node/express/server.js
+++ Node/express/server.js
@@ -8,6 +8,7 @@ const Users = [
 ]
 
 // 미들웨어
+app.use(express.json())
 app.use((req, res, next) => {
     const start = Date.now()
     console.log(`start: ${req.method} ${req.url}`)
@@ -24,6 +25,28 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
     res.send(Users)
+})
+
+app.post("/users", (req, res) => {
+    console.log("req.body.name: ", req.body.name)
+    // TypeError: Cannot read properties of undefined
+    // 전달된 body 값이 읽을 수 없는 형태이기 때문에 발생하는 이슈다. 이를 해결하기 위해서는 body의 값을 읽을 수 있는 형태로 파싱(추출)해야 한다.
+    // 이때 사용하는 게 body-parser라는 모듈인데, express 4.x 부터는 내장되어 있어 따로 설치하지 않아도 된다.
+    // 다만, 다음 코드를 추가해야 한다.
+    // app.use(express.json())
+
+    if (!req.body.name) {
+        return res.status(400).json({
+            error: "Missing User Name.",
+        })
+    }
+
+    const newUser = {
+        name: req.body.name,
+        id: Users.length,
+    }
+    Users.push(newUser)
+    res.json(newUser)
 })
 
 // req.params
